49e571ca02b5622c052130ab0c587a635fe52184
Moonstricked <waitlikewhatlol@gmail.com>
2026-09-12 13:00:35 +0800

    question 1 + some changes to index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,11 +5,28 @@ namespace PrefinalExamApp.Controllers
 {
     public class HomeController : Controller
     {
-        public static readonly List<ExamQuestions> Questions = new();
-
         public IActionResult Index()
         {
-            return View(Questions);
+            var questions = new List<ExamQuestions>
+            {
+                // these should all be like taken from
+                new ExamQuestions
+                {
+                    Id = 1,
+                    Question = "What is the main problem solved by using a database instead of an in-memory collection?",
+                    Options = new()
+                    {
+                        "A. It makes C# code shorter",
+                        "B. It prevents the application from restarting",
+                        "C. It allows data to persist after the application stops",
+                        "D. It removes the need for MVC"
+                    },
+                    CorrectAnswer = "C.",
+                    Explanation = "inmemory collections lose data upon application restart; relational databases provide storage that persists even after restarting."
+                }
+            };
+
+            return View(questions);
         }
     }
 }
