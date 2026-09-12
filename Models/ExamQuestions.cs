1f86484ff023d6efb52ab3ec095819e423a2a846
Moonstricked <waitlikewhatlol@gmail.com>
2026-09-12 12:52:01 +0800

    setting this up was harder than i thought

---
+++ Models/ExamQuestions.cs
@@ -0,0 +1,12 @@
+﻿namespace PrefinalExamApp.Models
+{
+    public class ExamQuestions
+    {
+        // this is so over the top but i think it's fiiiine??? i hope
+        public int Id { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public List<string> Options { get; set; } = new();
+        public string CorrectAnswer { get; set; } = string.Empty;
+        public string Explanation { get; set; } = string.Empty;
+    }
+}
