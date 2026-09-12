b22ec6040e82e5af5395aaf91b783620fae092e6
Moonstricked <waitlikewhatlol@gmail.com>
2026-09-12 12:54:05 +0800

    i nearly forgot (this is the final set-up step i hope)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,15 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PrefinalExamApp.Models;
 
-namespace PrefinalExamApp.Controllers;
-
-public class HomeController : Controller
+namespace PrefinalExamApp.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public static readonly List<ExamQuestions> Questions = new();
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View(Questions);
+        }
     }
 }
